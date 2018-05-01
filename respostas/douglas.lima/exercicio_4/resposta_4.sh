8bbbf8ade7571ffff9a915e46726b7de74ef8d49
Douglas Lima <douglaslima909@gmail.com>
2018-05-01 20:53:08 -0300

    Criei pasta com o meu login ccc

---
+++ respostas/douglas.lima/exercicio_4/resposta_4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Douglas Pereira de Lima - 117110636
+# Exercício 4
+#
+
+# Imprime a mensagem
+echo -n "Entre com a senha: "
+
+# Recebe da entrada a senha do usuário
+read senha;
+
+#variáveis de verificação
+contem_8=$false
+contem_letra_numeros=$false
+contem_maiuscula=$false
+
+#laço que percorre a senha para fazer verificações
+
+
+
