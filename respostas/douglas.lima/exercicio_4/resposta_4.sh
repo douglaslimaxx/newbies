8687564600d0394b0f35e892187757745988f240
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2018-05-02 15:20:56 -0300

    Resposta 4

--- respostas/douglas.lima/exercicio_4/resposta_4.sh
+++ respostas/douglas.lima/exercicio_4/resposta_4.sh
@@ -14,7 +14,19 @@ contem_8=$false
 contem_letra_numeros=$false
 contem_maiuscula=$false
 
+tamanho=${#senha}
+
+if [ $tamanho -ge 8 ]
+then
+	contem_8=$true
+	echo $"A senha contém tamanho 8 ou mais"
+fi
+
 #laço que percorre a senha para fazer verificações
 
+
+#if [ $contem_8 ]
+#then	
+#fi
 
 
